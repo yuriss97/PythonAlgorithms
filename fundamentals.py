f38e934a2fd7f7c633305faf9af9310bd16dfd81
Yuri Salvador <45977591+yuriss97@users.noreply.github.com>
2023-07-09 12:07:03 -0300

    Update fundamentals.py
    Added more exercises...

--- fundamentals.py
+++ fundamentals.py
@@ -88,3 +88,113 @@ while not isSorted:
 print(arrayOne)
 
 '''
+
+# 5)Write a program that accepts a sentence as input and outputs the number of words in the sentence.
+
+'''
+
+sentence = " How many words    does this sentence   has? "
+
+# Split the sentence into words
+numberOfWords = sentence.split()
+
+print(len(numberOfWords))
+
+'''
+
+# 6) Create a function that takes a list of integers as input and returns a new list with only the even numbers.
+
+''' 
+
+def returnEvenNumbers(listOfNumbers):
+    listOfEvenNumbers = []
+
+    # Looping thru all the elements from 'listOfNumbers'
+    for i in listOfNumbers:
+        # Checking if the number is even
+        if i % 2 == 0:
+            # If the number is even, add it to 'listOfEvenNumbers'
+            listOfEvenNumbers.append(i)
+
+    return listOfEvenNumbers
+
+listOfNumbers = [3,7,2,23,6,423,67,362,765,264,77]
+
+print(returnEvenNumbers(listOfNumbers))
+
+'''    
+
+# 7) Implement a function that checks if a given string is a palindrome (reads the same forwards and backwards).
+
+'''
+
+def isItPalindorme(word):
+    # Lenght of the word
+    wordLenght = len(word)
+
+    # Loop variable
+    i = 0
+
+    # Loop until middle letter is identified
+    while i < wordLenght / 2:
+        # Check if the opposite letter is the same
+        if word[i] != word[wordLenght - (i+1)]:
+            return False
+        i += 1
+    return True
+
+# Test cases
+word = "abcdeedcba"
+print(isItPalindorme(word))
+
+word = "abcdedcba"
+print(isItPalindorme(word))
+
+word = "abcdeedfcba"
+print(isItPalindorme(word))
+
+'''
+
+# 8) Implement a function that counts the frequency of each word in a given text and returns a dictionary with word counts.
+
+'''
+
+def count_word_frequency(text):
+    # Creating dictionary
+    word_counts = {}
+    # Converting text to lower case so the words are not case sensitive and using split to separate each word
+    words = text.lower().split()
+    
+    # Looping thru each word in words array
+    for word in words:
+        # If word is found in the dictionary, increment its value by one, if not, set value to one
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+    
+    return word_counts
+
+text = "This sentence is repeating some words and this sentence is repeating some words"
+frequency = count_word_frequency(text)
+print(frequency)
+
+'''
+
+# 9) Implement a function that return an address from a dictionary given a specific name
+
+'''
+
+addressDictionary = {'name1':'Boulevard One','name2':'Ealkwood Avenue','name3':'Elton Street'}
+
+def findAddress(name,adressDictionary):
+    if name in adressDictionary:
+        return adressDictionary[name]
+    else:
+        return "Name not found"
+    
+print(findAddress("name1",addressDictionary))
+print(findAddress("name2",addressDictionary))
+print(findAddress("name4",addressDictionary))
+
+'''
